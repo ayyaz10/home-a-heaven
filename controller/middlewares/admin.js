0aaff80c7dbdaa12c03a0d0f5eb5abc4473219a6
codementee <codementee@outlook.com>
2021-04-19 13:31:33 +0500

    update

--- controller/middlewares/admin.js
+++ controller/middlewares/admin.js
@@ -3,16 +3,11 @@ const { getAllUsers } = require('../../db/queries');
 const admin = async (req, res, next) => {
     try {
         const users = await getAllUsers();
-        // console.log(users.length)
-        // users.forEach(user => {
-
             for(let i = 0; i < users.length; i++) {
-                // console.log(JSON.parse(req.signedCookies.user_info).user_id)
-                // const userId = JSON.parse(req.signedCookies.user_info).user_id;
-                // const userRole = JSON.parse(req.signedCookies.user_info).role;
-                console.log(req.signedCookies.user_info)
                 if((typeof req.signedCookies.user_info !== "undefined")) {
-                    if(JSON.parse(req.signedCookies.user_info).user_id != users[i].user_id && JSON.parse(req.signedCookies.user_info).role != 'admin') {
+                    const userId = JSON.parse(req.signedCookies.user_info).user_id;
+                    const userRole = JSON.parse(req.signedCookies.user_info).role;
+                    if(userId != users[i].user_id && userRole != 'admin') {
                         return res.redirect('/')
                     } else {
                         return next();
@@ -22,7 +17,6 @@ const admin = async (req, res, next) => {
                 }
 
             }
-        // })
     } catch (error) {
         console.error(error)
     }
