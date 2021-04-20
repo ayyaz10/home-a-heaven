25c8e7fd32be1bb0e4783d2b97c1a6ff55ebfcc1
codementee <codementee@outlook.com>
2021-04-21 00:25:17 +0500

    dicount price added front end

--- controller/middlewares/admin.js
+++ controller/middlewares/admin.js
@@ -1,22 +1,24 @@
-const { getAllUsers } = require('../../db/queries');
+const {
+    getAllUsers
+} = require('../../db/queries');
 
 const admin = async (req, res, next) => {
     try {
         const users = await getAllUsers();
-            for(let i = 0; i < users.length; i++) {
-                if((typeof req.signedCookies.user_info !== "undefined")) {
-                    const userId = JSON.parse(req.signedCookies.user_info).user_id;
-                    const userRole = JSON.parse(req.signedCookies.user_info).role;
-                    if(userId != users[i].user_id && userRole != 'admin') {
-                        return res.redirect('/')
-                    } else {
-                        return next();
-                    }
-                } else {
+        for (let i = 0; i < users.length; i++) {
+            if ((typeof req.signedCookies.user_info !== "undefined")) {
+                const userId = JSON.parse(req.signedCookies.user_info).user_id;
+                const userRole = JSON.parse(req.signedCookies.user_info).role;
+                if (userId != users[i].user_id && userRole != 'admin') {
                     return res.redirect('/')
+                } else {
+                    return next();
                 }
-
+            } else {
+                return res.redirect('/')
             }
+
+        }
     } catch (error) {
         console.error(error)
     }
