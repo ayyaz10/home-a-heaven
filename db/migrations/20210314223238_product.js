d9db6b2396f69e5189f7bed0ddf9f2ec0693b9d3
codementee <codementee@outlook.com>
2021-04-22 23:40:12 +0500

    update

--- db/migrations/20210314223238_product.js
+++ db/migrations/20210314223238_product.js
@@ -7,11 +7,11 @@ exports.up = function(knex) {
         // table.integer('admin_id').unsigned();
         // table.string('category_id', 255).notNullable();
         table.string('product_name', 255).notNullable();
-        table.string('price', 255).unsigned().notNullable();
+        table.integer('price', 255).unsigned().notNullable();
         table.string('category_name', 255).notNullable();
         table.string('sub_cat_name', 255);
         table.integer('inStock').unsigned().notNullable();
-        table.string('discount').unsigned();
+        table.integer('discount').unsigned();
         // table.string('cat_id', 255).notNullable();
         table.string('subcat_id', 255);
         table.string('image', 255).notNullable();
